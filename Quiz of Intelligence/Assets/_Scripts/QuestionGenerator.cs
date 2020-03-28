5d02e25f142bf7b3510ff863689ae715e0089f2a
Jawad Ali <alijamil734@gmail.com>
2020-03-28 12:40:39 +0500

    implemenet: timer to solve a questions with slider progress

--- Quiz of Intelligence/Assets/_Scripts/QuestionGenerator.cs
+++ Quiz of Intelligence/Assets/_Scripts/QuestionGenerator.cs
@@ -11,13 +11,32 @@ public class QuestionGenerator : MonoBehaviour
 {
     #region Texts
 
+    [Header("Questions and options texts")]
     public TextMeshProUGUI questionText;
+
     public TextMeshProUGUI[] answerTexts;
+
+    [Space(12)] [Header("timer components/items")]
+    public TextMeshProUGUI timer_text;
+
+    public Image timer_slider;
 
     #endregion
 
+    // Create a property to handle the slider's value
+    private float currentValue = 0f;
+
+    /// <summary>
+    /// Max time to solve a questions 
+    /// </summary>
+    [SerializeField] [Header("Max time to solve a questions")]
+    private float maxValue = 5f;
+
+    private float minValue = 0f;
+
     public static int currentQuestion;
-    private Questions ques;
+    private Questions questionObject;
+    private Question ques;
     private string json;
 
     private List<int> wrongAnswersListForHint = new List<int>();
@@ -37,27 +56,41 @@ public class QuestionGenerator : MonoBehaviour
     {
         // get json file
         json = File.ReadAllText(Application.dataPath + "/output.json");
-        ques = JsonUtility.FromJson<Questions>(json);
+        questionObject = JsonUtility.FromJson<Questions>(json);
         // get question when quiz starts
         GetQuestion();
     }
 
+    private void FixedUpdate()
+    {
+        // start timer only when question is fully loaded
+        if (QuestionsAnimations.gameStarted)
+        {
+            // timer starts
+            CurrentValue += Time.deltaTime;
+            // check if user can not answer in given time, its wrong answer man
+            if (CurrentValue >= maxValue)
+            {
+                AnswerResponse(false);
+            }
+        }
+    }
 
     /// <summary>
     /// to get questions
     /// </summary>
     void GetQuestion()
     {
-        var q = ques.questions[currentQuestion];
+        ques = questionObject.questions[currentQuestion];
 
         // question's text
-        questionText.text = q.question;
+        questionText.text = ques.question;
         // shuffle answers list
-        q.answers = q.answers.Shuffle().ToList();
+        ques.answers = ques.answers.Shuffle().ToList();
         // answers text
-        for (var i = 0; i < q.answers.Count; i++)
+        for (var i = 0; i < ques.answers.Count; i++)
         {
-            answerTexts[i].text = q.answers[i].answerText;
+            answerTexts[i].text = ques.answers[i].answerText;
 
             // populate wrong answer
             // if (!q.answers[i].isCorrect)
@@ -77,20 +110,77 @@ public class QuestionGenerator : MonoBehaviour
     {
         GetQuestion();
         QuestionsAnimations.instance.AnimateQuestions_IN(0.2f);
+        Reset();
     }
 
+    private void Reset()
+    {
+        CurrentValue = 0;
+    }
+
+
     /// <summary>
-    /// Answer's response
+    /// Get time since questions loaded
     /// </summary>
-    /// <param name="isCorrect">true for correct answer</param>
-    /// <param name="index">index of option</param>
-    void AnswerResponse(bool isCorrect, int index)
+    public float CurrentValue
+    {
+        get { return currentValue; }
+        set
+        {
+            // Ensure the passed value falls within min/max range
+            currentValue = Mathf.Clamp(value, minValue, maxValue);
+
+            // Calculate the current fill percentage and display it
+            float fillPercentage = currentValue / maxValue;
+            timer_slider.fillAmount = fillPercentage;
+
+            float countDown = maxValue - currentValue;
+            timer_text.text = countDown.ToString("f0");
+        }
+    }
+
+
+    /// <summary>
+    /// Called on response of every question
+    /// </summary>
+    /// <param name="isCorrect">true if answer is correct</param>
+    void AnswerResponse(bool isCorrect)
     {
         QuestionsAnimations.gameStarted = false;
         currentQuestion++;
         // go to next question with some delay
         StartCoroutine(WaitAndAnimateQuestionsOUT());
 
+
+        if (isCorrect)
+        {
+            // correct answer
+        }
+        else
+        {
+            // wrong answer
+
+            // if user gave wrong answer, show him the correct answer
+            for (int i = 0; i < ques.answers.Count; i++)
+            {
+                if (ques.answers[i].isCorrect)
+                {
+                    var a = answerTexts[i].transform.parent.GetChild(1);
+                    a.gameObject.SetActive(true);
+                    a.GetComponent<Image>().color = Color.green;
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Show green or red sprite, to show user answer response.
+    /// Overloading only to show correct/wrong answer sprite, only called if user answered on time 
+    /// </summary>
+    /// <param name="isCorrect">true if user answered correct</param>
+    /// <param name="index">index of option</param>
+    void AnswerResponse(bool isCorrect, int index)
+    {
         // give user response about his answer
         var image = answerTexts[index].transform.parent.GetChild(1);
         image.gameObject.SetActive(true);
@@ -99,13 +189,17 @@ public class QuestionGenerator : MonoBehaviour
         {
             // its correct answer man
             image.GetComponent<Image>().color = Color.green;
+
+            AnswerResponse(true);
         }
         else
         {
             // its wrong answer man
             image.GetComponent<Image>().color = Color.red;
+            AnswerResponse(false);
         }
     }
+
 
     IEnumerator WaitAndAnimateQuestionsOUT()
     {
@@ -118,7 +212,7 @@ public class QuestionGenerator : MonoBehaviour
     {
         if (QuestionsAnimations.gameStarted)
         {
-            Question questionObj = ques.questions[currentQuestion];
+            Question questionObj = ques;
             switch (index)
             {
                 case 1:
